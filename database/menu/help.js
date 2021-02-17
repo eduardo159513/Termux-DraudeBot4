317d15226f4070e72a5855bea0614b4200c3f8d1
eduardo159513 <76506515+eduardo159513@users.noreply.github.com>
2021-02-17 16:48:02 -0300

    Update help.js

--- database/menu/help.js
+++ database/menu/help.js
@@ -1,10 +1,10 @@
-const help = (botPrefix, ownerName, botName, pushname, reqXp, uangku) => {
+const help = (prefix, ownerName, botName, pushname, reqXp, uangku) => {
         return `
 ╔══✪〘 Informações 〙✪════
 ║
 ║───────⊹⊱✫⊰⊹─────
 ║➩ ❍ wa.me/5566996149601
-║➩ ❍ Prefix: 「  ${botPrefix}  」
+║➩ ❍ Prefix: 「  ${prefix}  」
 ║➩ ❍ Criador: ${ownerName}
 ║➩ ❍ Bot: ${botName}
 ║➩ ❍ Nome: ${pushname}️
@@ -19,34 +19,34 @@ const help = (botPrefix, ownerName, botName, pushname, reqXp, uangku) => {
 ╔══✪〘 Comandos Do Bot〙✪══
 ║
 ║───────⊹⊱✫⊰⊹──────
-║➩ ❍ *${botPrefix}info*
-║➩ ❍ *${botPrefix}blocklist*
-║➩ ❍ *${botPrefix}chatlist*
-║➩ ❍ *${botPrefix}ping*
-║➩ ❍ *${botPrefix}bugreport*
-║➩ ❍ *${botPrefix}logomaker*
-║➩ ❍ *${botPrefix}imagemaker*
-║➩ ❍ *${botPrefix}stickermaker*
-║➩ ❍ *${botPrefix}search*
-║➩ ❍ *${botPrefix}education*
-║➩ ❍ *${botPrefix}kerang*
-║➩ ❍ *${botPrefix}downloader*
-║➩ ❍ *${botPrefix}meme*
-║➩ ❍ *${botPrefix}group*
-║➩ ❍ *${botPrefix}sound*
-║➩ ❍ *${botPrefix}music*
-║➩ ❍ *${botPrefix}islam*
-║➩ ❍ *${botPrefix}stalk*
-║➩ ❍ *${botPrefix}wibu*
-║➩ ❍ *${botPrefix}18+*
-║➩ ❍ *${botPrefix}fun*
-║➩ ❍ *${botPrefix}tod*
-║➩ ❍ *${botPrefix}information*
-║➩ ❍ *${botPrefix}stayonscreen*
-║➩ ❍ *${botPrefix}xp*
-║➩ ❍ *${botPrefix}limit*
-║➩ ❍ *${botPrefix}owner*
-║➩ ❍ *${botPrefix}other*
+║➩ ❍ *${prefix}info*
+║➩ ❍ *${prefix}blocklist*
+║➩ ❍ *${prefix}chatlist*
+║➩ ❍ *${prefix}ping*
+║➩ ❍ *${prefix}bugreport*
+║➩ ❍ *${prefix}logomaker*
+║➩ ❍ *${prefix}imagemaker*
+║➩ ❍ *${prefix}stickermaker*
+║➩ ❍ *${prefix}search*
+║➩ ❍ *${prefix}education*
+║➩ ❍ *${prefix}kerang*
+║➩ ❍ *${prefix}downloader*
+║➩ ❍ *${prefix}meme*
+║➩ ❍ *${prefix}group*
+║➩ ❍ *${prefix}sound*
+║➩ ❍ *${prefix}music*
+║➩ ❍ *${prefix}islam*
+║➩ ❍ *${prefix}stalk*
+║➩ ❍ *${prefix}wibu*
+║➩ ❍ *${prefix}18+*
+║➩ ❍ *${prefix}fun*
+║➩ ❍ *${prefix}tod*
+║➩ ❍ *${prefix}information*
+║➩ ❍ *${prefix}stayonscreen*
+║➩ ❍ *${prefix}xp*
+║➩ ❍ *${prefix}limit*
+║➩ ❍ *${prefix}owner*
+║➩ ❍ *${prefix}other*
 ║➩ Aumente seu level interagindo no grupo!!
 ║───────⊹⊱✫⊰⊹───────`
 }
